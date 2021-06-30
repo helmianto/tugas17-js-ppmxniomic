5f92cf36068352588cb5b126cbc74fee102ae72d
Helmianto <helmianto15@gmail.com>
2021-06-30 16:38:13 +0700

    initial commit

---
+++ tugas17-1.js
@@ -0,0 +1,7 @@
+const word1 = 'Saya beLajar bahaSa peMrograman deNgan khUsuk';
+const word2 = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.';
+
+console.log(word1.toLowerCase());
+console.log(word1.toUpperCase());
+console.log(word2.toLowerCase());
+console.log(word2.toUpperCase());
---
+++ tugas17-2.js
@@ -0,0 +1,9 @@
+const word = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const ascii1 = word.charCodeAt(0);
+const ascii2 = word.charCodeAt(1);
+const ascii3 = word.charCodeAt(2);
+const ascii4 = word.charCodeAt(3);
+const ascii5 = word.charCodeAt(4);
+const ascii6 = word.charCodeAt(5);
+
+console.log(`${ascii1}-${ascii2}-${ascii3}-${ascii4}-${ascii5}-${ascii6}`);
